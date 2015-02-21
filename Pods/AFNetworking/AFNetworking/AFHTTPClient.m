dc027ba17baeb6147d4b372426fa920cd6191e3f
Scott Null <scott.null@gmail.com>
2015-02-20 22:30:47 -0500

    Zero Warnings but now chat bubbles are off

--- Pods/AFNetworking/AFNetworking/AFHTTPClient.m
+++ Pods/AFNetworking/AFNetworking/AFHTTPClient.m
@@ -697,7 +697,7 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
     }
 
     self.stringEncoding = [aDecoder decodeIntegerForKey:@"stringEncoding"];
-    self.parameterEncoding = [aDecoder decodeIntegerForKey:@"parameterEncoding"];
+    self.parameterEncoding = *((AFHTTPClientParameterEncoding *)[aDecoder decodeIntegerForKey:@"parameterEncoding"]);
     self.registeredHTTPOperationClassNames = [aDecoder decodeObjectForKey:@"registeredHTTPOperationClassNames"];
     self.defaultHeaders = [aDecoder decodeObjectForKey:@"defaultHeaders"];
 
@@ -735,7 +735,7 @@ static NSString * const kAFMultipartFormBoundary = @"Boundary+0xAbCdEfGbOuNdArY"
 
 static NSString * const kAFMultipartFormCRLF = @"\r\n";
 
-static NSInteger const kAFStreamToStreamBufferSize = 1024 * 1024; //1 meg default
+//static NSInteger const kAFStreamToStreamBufferSize = 1024 * 1024; //1 meg default
 
 static inline NSString * AFMultipartFormInitialBoundary() {
     return [NSString stringWithFormat:@"--%@%@", kAFMultipartFormBoundary, kAFMultipartFormCRLF];
