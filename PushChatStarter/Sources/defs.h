79e151bd4d398c5c9d4ebe17b13f03251ef5efb9
Scott Null <scott.null@gmail.com>
2026-03-06 23:56:25 -0500

    initial REBOOT commit

--- PushChatStarter/Sources/defs.h
+++ PushChatStarter/Sources/defs.h
@@ -8,8 +8,8 @@
 // for local testing use the next line
 //#define ServerApiURL @"http://10.0.0.27:44447"
 // for the real deal on bluehost use the next line
-#define ServerApiURL @"https://www.altcoinfolio.com"
-//#define ServerApiURL @"http://ec2-54-214-183-228.us-west-2.compute.amazonaws.com"
+//#define ServerApiURL @"https://www.altcoinfolio.com"
+#define ServerApiURL @"http://ec2-54-245-7-246.us-west-2.compute.amazonaws.com"
 
 //Test
 //#define ServerPostPathURL @"/whereru/api/api.php"
